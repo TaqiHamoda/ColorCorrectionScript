066f7c3f77a2fe21d237ed9a26df62ac8db17a29
Taqi Hamoda <taqihisham@gmail.com>
2025-02-03 14:37:07 +0100

    fixed gray world

--- main.py
+++ main.py
@@ -24,8 +24,45 @@ def contrast_enhancement(image, kernel_size=5, clip_limit=2.0):
     return cv2.cvtColor(hsv_image, cv2.COLOR_HSV2BGR)
 
 
-def white_balance(image, gain):
+def white_balance_gray_world(image):
+    # Source: https://www.researchgate.net/publication/235350557_Combining_Gray-World_assumption_White-Point_correction_and_power_transformation_for_automatic_white_balance
+
     image_out = image.astype(np.float32)
+
+    mean_r = np.mean(image_out[:, :, 2])
+    mean_g = np.mean(image_out[:, :, 1])
+    mean_b = np.mean(image_out[:, :, 0])
+
+    # Mean value of all channels
+    gain = np.mean((mean_r, mean_g, mean_b)) / 255
+
+    max_r = np.max(image_out[:, :, 2])
+    max_g = np.max(image_out[:, :, 1])
+    max_b = np.max(image_out[:, :, 0])
+
+    # logarithm base to which each channel will be raised
+    base_r = mean_r / max_r
+    base_g = mean_g / max_g
+    base_b = mean_b / max_b
+    
+    # the power to which each channel will be raised
+    power_r = np.log(gain) / np.log(base_r)
+    power_g = np.log(gain) / np.log(base_g)
+    power_b = np.log(gain) / np.log(base_b)
+    
+    # separately applying different color correction powers to each channel
+    image_out[:, :, 2] = (image_out[:, :, 2] / max_r) ** power_r
+    image_out[:, :, 1] = (image_out[:, :, 1] / max_g) ** power_g
+    image_out[:, :, 0] = (image_out[:, :, 0] / max_b) ** power_b
+
+    return (255 * image_out).astype(np.uint8)
+
+
+def white_balance_percentile(image, percentile=97.5):
+    image_out = image.astype(np.float32)
+
+    # Calculate the percentile values for each channel
+    gain = 255 / np.percentile(image, percentile, axis=(0, 1))
 
     # Apply the gain to each color channel
     image_out[:, :, 2] *= gain[2]
@@ -35,32 +72,96 @@ def white_balance(image, gain):
     return np.clip(image_out, 0, 255).astype(np.uint8)
 
 
-def white_balance_patch(image, patch_column, patch_row, patch_width, patch_height):
-    # Extract the "true" white patch from the image
-    patch = image[patch_row: patch_row + patch_height, patch_column: patch_column + patch_width]
-    mean_color = np.mean(patch, axis=(0, 1))
-
-    return white_balance(image, 255 / mean_color)
-
-
-def white_balance_percentile(image, percentile=97.5):
-    # Calculate the percentile values for each channel
-    percentile_values = np.percentile(image, percentile, axis=(0, 1))
-
-    return white_balance(image, 255 / percentile_values)
-
-
-def white_balance_gray_world(image):
-    gain = np.mean(image) / np.mean(image, axis=(0, 1))
-
-    return white_balance(image, gain)
-
-
 def denoise(image, diameter=5, sigmaColor=50, sigmaSpace=50):
     return cv2.bilateralFilter(image, diameter, sigmaColor, sigmaSpace)
 
 
+# def exposure_ratio(image, window_size=5, sigma=0.5, lambda_factor=0.001, epsilon=1e-6):
+#     # Source: https://www.mdpi.com/1424-8220/20/16/4378
+
+#     # Extract Value Channel (L) from HSV transformation
+#     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+#     L = hsv_image[:, :, 2].astype(np.float64) / 255.0  # Normalize L to [0, 1]
+
+#     # Define Gradient Operator (∆D)
+#     dx = cv2.Sobel(L, cv2.CV_64F, 1, 0, ksize=3)
+#     dy = cv2.Sobel(L, cv2.CV_64F, 0, 1, ksize=3)
+#     delta_DL = np.sqrt(dx**2 + dy**2)  # Gradient magnitude
+
+#     # Define Gaussian Kernel (Gσ) - Example: σ = 1/2
+#     gaussian_kernel = cv2.getGaussianKernel(window_size, sigma)  # Adjust kernel size as needed
+#     gaussian_delta_dl = cv2.filter2D(delta_DL, -1, gaussian_kernel @ gaussian_kernel.T)
+
+#     WD = 1 / (np.abs(gaussian_delta_dl) + epsilon)
+#     WD = gaussian_kernel / WD
+
+#     balancing_coefficient = 0
+
+#     H, W = L.shape
+#     for y in range(H):
+#         for x in range(W):
+#             y_min = max(0, y - window_size)
+#             y_max = min(H - 1, y + window_size)
+#             x_min = max(0, x - window_size)
+#             x_max = min(W - 1, x + window_size)
+
+#             window_delta_DL = delta_DL[y_min:y_max+1, x_min:x_max+1]
+#             balancing_coefficient += WD / (np.linalg.norm(window_delta_DL) + epsilon)
+
+#     T = np.sum(L - lambda_factor * balancing_coefficient/2)
+
+#     return 1 / np.maximum(T, epsilon)
+
+
+# def single_image_hdr(image):
+#     # Apply logarithmic transformation to the input image to bound the irradiance information to a range from 0 to 1
+#     max_val = np.max(image)
+#     log_image = np.log(image + 1) / np.log(max_val + 1)
+
+#     # Apply RGB-to-HSV transformation to the log image
+#     hsv_image = cv2.cvtColor((log_image * 255.0).astype(np.uint8), cv2.COLOR_BGR2HSV)
+
+#     exposure_ratio()
+
+
 if __name__ == '__main__':
+    # import matplotlib.pyplot as plt
+
+    # image = cv2.imread("images/uxo_enhanced.jpg")
+
+    # kernel = 5
+
+    # results = []
+    # for i in range(6):
+    #     results.append([])
+
+    #     clip = 1.0
+    #     for j in range(5):
+    #         results[-1].append(contrast_enhancement(image, kernel_size=kernel, clip_limit=clip))
+    #         clip += 1
+
+    #     kernel += 5
+
+    # fig, axes = plt.subplots(nrows=len(results), ncols=len(results[0]), figsize=(15, 10))
+
+    # kernel = 5
+    # for i, row in enumerate(results):
+    #     clip = 1
+    #     for j, image in enumerate(row):
+    #         axes[i, j].imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    #         axes[i, j].set_title(f"Kernel: {kernel}, Clip: {clip}")
+    #         axes[i, j].axis('off')
+
+    #         clip += 1
+
+    #     kernel += 5
+
+    # plt.tight_layout()
+    # plt.show()
+
+    # exit()
+
+
     # Parse command line arguments
     parser = argparse.ArgumentParser(description='Image enhancement script')
     parser.add_argument('--input_folder', type=str, required=True, help='Path to the input folder with images')
@@ -104,7 +205,7 @@ if __name__ == '__main__':
 
     # Load images from input folder
     for filename in os.listdir(args.input_folder):
-        if filename.endswith(('.jpg', '.png')): # More concise way to check multiple extensions
+        if filename.endswith(('.jpg', '.png', '.jpeg')): # More concise way to check multiple extensions
             image_path = os.path.join(args.input_folder, filename)
             image = cv2.imread(image_path)
 
@@ -113,20 +214,18 @@ if __name__ == '__main__':
 
             # Apply white balancing
             wb_type = config['white_balance_type']
-            if wb_type == 'patch':
-                image = white_balance_patch(image, config['white_balance_patch_column'], config['white_balance_patch_row'], config['white_balance_patch_width'], config['white_balance_patch_height'])
-            elif wb_type == 'percentile':
+            if wb_type == 'percentile':
                 image = white_balance_percentile(image, config['white_balance_percentile'])
             elif wb_type == 'gray_world':
                 image = white_balance_gray_world(image)
 
-            # Apply CLAHE
-            if not config['skip_clahe']:
-                image = contrast_enhancement(image, config['clahe_kernel_size'], config['clahe_clip_limit'])
-
             # Apply denoising
             if not config['skip_denoising']:
                 image = denoise(image, config['denoise_diameter'], config['denoise_sigma_color'], config['denoise_sigma_space'])
+
+            # Apply CLAHE
+            if not config['skip_clahe']:
+                image = contrast_enhancement(image, config['clahe_kernel_size'], config['clahe_clip_limit'])
 
             # Save output image
             name, ext = os.path.splitext(filename)
